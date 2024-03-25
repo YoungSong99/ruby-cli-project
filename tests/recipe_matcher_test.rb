b9c61243607dd9d07f675afc5dd5cbd9a136d254
youngsong99 <hyangyoungs@gmail.com>
2024-03-25 13:23:04 -0500

    fix: test file value update

--- tests/recipe_matcher_test.rb
+++ tests/recipe_matcher_test.rb
@@ -4,7 +4,7 @@ require_relative '../recipe_matcher'
 class RecipeMatcherTest < Minitest::Test
   def test_find_recipes
     matcher = RecipeMatcher.new(["pasta", "tomato sauce", "salt", "potato"])
-    expected = { "Spaghetti" => 100.0, "Potato Salad" => 33.33, "Chicken Salad" => 0.0 }
+    expected = { "Spaghetti" => 100.0, "Potato Salad" => 33.33, "Beef Stew" => 20.0}
     assert_equal expected, matcher.find_recipes
   end
 end
