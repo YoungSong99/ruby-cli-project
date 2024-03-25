a0673cfdd2ad79480e87ba898e5db076306f16c5
youngsong99 <hyangyoungs@gmail.com>
2024-03-24 22:49:21 -0500

    feat: add see_ingredients

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
 require_relative 'recipe_matcher'
 
-puts "Please enter the ingredients you have (separated by commas):"
-input = gets.chomp
-ingredients = input.split(",").map(&:strip)
+matcher = RecipeMatcher.new
+matcher.run
 
-matcher = RecipeMatcher.new(ingredients)
-matcher.print_matches
--- recipe_matcher.rb
+++ recipe_matcher.rb
@@ -1,33 +1,83 @@
-# recipe_matcher.rb
-
 class RecipeMatcher
-  attr_accessor :ingredients
+  RECIPES = {
+    "Spaghetti" => ["pasta", "tomato sauce", "salt"],
+    "Potato Salad" => ["potato", "mayonnaise", "onion"],
+    "Chicken Salad" => ["chicken", "lettuce", "dressing"],
+    "Beef Stew" => ["beef", "carrot", "potato", "onion", "celery"],
+    "Vegetarian Chili" => ["bean", "tomato", "onion", "bell pepper", "chili powder"],
+    "Fish Tacos" => ["fish", "tortilla", "cabbage", "lime", "avocado"],
+    "Pancakes" => ["flour", "egg", "milk", "butter", "sugar"],
+    "Caesar Salad" => ["lettuce", "crouton", "parmesan cheese", "Caesar dressing"],
+    "BBQ Chicken Pizza" => ["pizza dough", "chicken", "bbq sauce", "mozzarella cheese", "red onion"],
+    "Mushroom Risotto" => ["rice", "mushroom", "chicken broth", "parmesan cheese", "onion"],
+    "Tomato Soup" => ["tomato", "chicken broth", "onion", "carrot", "celery"]
+  }
 
   def initialize(ingredients = [])
     @ingredients = ingredients
   end
 
-  def find_recipes
-    recipes = {
-      "Spaghetti" => ["pasta", "tomato sauce", "salt"],
-      "Potato Salad" => ["potato", "mayonnaise", "onion"],
-      "Chicken Salad" => ["chicken", "lettuce", "dressing"]
-    }
+  def see_ingredients
+    puts "Available ingredients:"
+    RECIPES.values.flatten.uniq.sort.each { |ingredient| puts ingredient }
+  end
 
+  def enter_ingredients
+    puts "Enter your ingredients (type 'done' when finished):"
+    loop do
+      ingredient = gets.chomp.downcase
+      break if ingredient == 'done'
+      @ingredients << ingredient
+      puts "#{ingredient} added."
+    end
+  end
+
+  def print_matches
+    puts "Possible recipe list based on current ingredients:"
+    matches = find_recipes
+    if matches.empty?
+      puts "No matches found."
+    else
+      matches.each do |recipe, percentage|
+        puts "#{recipe}: #{percentage}% match"
+      end
+    end
+  end
+
+  def find_recipes
     matches = {}
 
-    recipes.each do |name, recipe_ingredients|
+    RECIPES.each do |name, recipe_ingredients|
       match_percentage = (recipe_ingredients & @ingredients).count.to_f / recipe_ingredients.count
-      matches[name] = (match_percentage * 100).round(2)
+      matches[name] = (match_percentage * 100).round(2) if match_percentage > 0
     end
 
     matches.sort_by { |_, percentage| -percentage }.to_h
   end
 
-  def print_matches
-    puts "Possible recipe list:"
-    find_recipes.each do |recipe, percentage|
-      puts "#{recipe}: #{percentage}% match"
+  def run
+    loop do
+      puts "\nWelcome to the Recipe Matcher!"
+      puts "1. List all Ingredients"
+      puts "2. Enter the ingredients you have"
+      puts "3. See the possible recipes based on the ingredients you have"
+      puts "4. Exit"
+      print "Choose an option: "
+      option = gets.chomp.to_i
+      puts "\n"
+      case option
+      when 1
+        see_ingredients
+      when 2
+        enter_ingredients
+      when 3
+        print_matches
+      when 4
+        puts "Exiting..."
+        break
+      else
+        puts "Invalid option."
+      end
     end
   end
 end
